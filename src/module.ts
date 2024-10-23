4175462edf110f3a4a168ad6e08cc584649a3ba8
Sybren W <sybren.willemot@gmail.com>
2024-10-23 21:45:04 +0200

    feat(auto-import): set imports under right category and use right util fn

--- src/module.ts
+++ src/module.ts
@@ -18,18 +18,39 @@ export default defineNuxtModule<ModuleOptions>({
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    for (const [name] of Object.entries(ArkUI)) {
-      if (name[0] === name[0].toUpperCase()) {
-        // Only add components that start with a capital letter
-        addComponent({
-          name,
-          export: name,
-          filePath: '@ark-ui/vue',
-        })
-      }
+    // List of Ark UI components (e.g. AvatarRoot, AvatarImage, etc.)
+    const arkComponents: [string, object][] = Object.entries(ArkUI).filter(([name, component]) =>
+      name[0] === name[0].toUpperCase()
+      && typeof component === 'object'
+      && 'setup' in component,
+    )
+
+    // List of Ark UI component groups (e.g. Avatar, Button, etc.) used for the dot-notation (e.g. Avatar.Root)
+    const arkComponentGroups = Object.entries(ArkUI).filter(([name, value]) =>
+      name[0] === name[0].toUpperCase()
+      && typeof value === 'object'
+      && Object.values(value).some(component =>
+        typeof component === 'object' && 'setup' in component,
+      ),
+    ).map(([name]) => name)
+
+    // Add Ark components to the auto-imports
+    for (const [name] of arkComponents) {
+      addComponent({
+        name,
+        export: name,
+        filePath: '@ark-ui/vue',
+      })
     }
 
-    // Add imports for the utils.
+    // Add Ark component groups to the auto-imports
+    // This are imports since they are objects containing Vue components.
+    addImportsSources({
+      from: '@ark-ui/vue',
+      imports: arkComponentGroups,
+    })
+
+    // Add imports for the utils. Follows this list: https://github.com/chakra-ui/ark/blob/334cf29dc854d6f8af1e8a240610d8e5bcc63145/packages/vue/src/index.ts#L3
     addImportsSources({
       from: '@ark-ui/vue',
       imports: [
